8f80c7b8b648fc74f42e833e5536ebf5a5614974
gunners6518 <gunners6518@gmail.com>
2022-01-14 09:36:56 +0900

    creat new joke

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,3 +1,21 @@
+import { ActionFunction, redirect } from "remix";
+import { db } from "~/utils/db.server";
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const content = form.get("content");
+  const name = form.get("name");
+
+  // バリデーション
+  if (typeof name !== "string" || typeof content !== "string") {
+    throw new Error("Form not submit correctly");
+  }
+
+  const fields = { content, name };
+  const joke = await db.joke.create({ data: fields });
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJokeRoute() {
   return (
     <div>
